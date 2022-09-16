ee78d9caa0beaee113214746d7e612f38dbdb30a
daniil-korzun-idf <daniil.korzun@idftechnology.by>
2022-09-16 19:18:41 +0300

    feat(init): initial commit

---
+++ src/main/kotlin/by/korzun/cryptocurrencywatcherkotlin/CryptocurrencyWatcherKotlinApplication.kt
@@ -0,0 +1,11 @@
+package by.korzun.cryptocurrencywatcherkotlin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CryptocurrencyWatcherKotlinApplication
+
+fun main(args: Array<String>) {
+	runApplication<CryptocurrencyWatcherKotlinApplication>(*args)
+}
